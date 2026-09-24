076cd2ae496d23ed8810a36db0c1c28313debc18
Riccardo Menegazzo <52837869+riccardomenegazzo@users.noreply.github.com>
2026-09-24 22:32:59 +0200

    Preserve stdio HTTP compatibility while tightening remote URLs

--- internal/config/config.go
+++ internal/config/config.go
@@ -66,6 +66,9 @@ func (c *Config) Validate() error {
 	if c.Transport == "stdio" {
 		return nil
 	}
+	if err := requireSecureURL("SYSDIG_MCP_API_HOST", apiHost); err != nil {
+		return err
+	}
 	if err := validateSecureURL("SYSDIG_MCP_API_HOST", apiHost); err != nil {
 		return err
 	}
